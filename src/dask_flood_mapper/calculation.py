222d8e3a5d9240c684667ed4d75a76e2e494711a
npikall <nikolas.pikall@tuwien.ac.at>
2025-07-23 21:33:36 +0200

    [style] added typehints
    - added typehints
    - fmt and lint with ruff (isort and black compatible)
    - minor readability changes

--- src/dask_flood_mapper/calculation.py
+++ src/dask_flood_mapper/calculation.py
@@ -1,71 +1,74 @@
+"""Functions to calculate flood-related metrics."""
+
 import numpy as np
 import xarray as xr
 
 
-def calculate_flood_dc(sig0_dc, plia_dc, hpar_dc):
-    """
-    Merge four data cubes and apply processing steps to clean
+def calculate_flood_dc(
+    sig0_dc: xr.DataArray | xr.Dataset,
+    plia_dc: xr.DataArray | xr.Dataset,
+    hpar_dc: xr.DataArray | xr.Dataset,
+) -> xr.Dataset:
+    """Merge three data cubes.
+
+    Merge three data cubes and apply processing steps to clean
     and filter the dataset. wcover_dc is optional.
     """
-    flood_dc = xr.merge([sig0_dc, plia_dc, hpar_dc])
-    flood_dc = (
+    # TODO: Add wcover_dc to the calculation  # noqa: FIX002
+    flood_dc: xr.Dataset = xr.merge([sig0_dc, plia_dc, hpar_dc])
+    flood_dc: xr.Dataset = (
         flood_dc.reset_index("orbit", drop=True)
         .rename({"orbit": "time"})
         .dropna(dim="time", how="all", subset=["sig0"])
     )
-    flood_dc = flood_dc.persist()
-    return flood_dc
+    return flood_dc.persist()
 
 
-def calc_water_likelihood(dc):
+def calc_water_likelihood(dc: xr.Dataset) -> xr.DataArray:
+    """Calculate the water likelihood based on the MPLIA value."""
     return dc.MPLIA * -0.394181 + -4.142015
 
 
-def harmonic_expected_backscatter(dc):
-    w = np.pi * 2 / 365
+def harmonic_expected_backscatter(dc: xr.Dataset) -> xr.DataArray:
+    """Calculate the harmonic expected backscatter."""
+    w: float = np.pi * 2 / 365
 
-    t = dc.time.dt.dayofyear
-    wt = w * t
+    t: xr.DataArray = dc.time.dt.dayofyear
+    wt: xr.DataArray = w * t
 
-    M0 = dc.M0
-    S1 = dc.S1
-    S2 = dc.S2
-    S3 = dc.S3
-    C1 = dc.C1
-    C2 = dc.C2
-    C3 = dc.C3
-    hm_c1 = (M0 + S1 * np.sin(wt)) + (C1 * np.cos(wt))
-    hm_c2 = (hm_c1 + S2 * np.sin(2 * wt)) + C2 * np.cos(2 * wt)
-    hm_c3 = (hm_c2 + S3 * np.sin(3 * wt)) + C3 * np.cos(3 * wt)
-    return hm_c3
+    hm_c1: xr.DataArray = (dc.M0 + dc.S1 * np.sin(wt)) + (dc.C1 * np.cos(wt))
+    hm_c2: xr.DataArray = (hm_c1 + dc.S2 * np.sin(2 * wt)) + dc.C2 * np.cos(2 * wt)
+    return (hm_c2 + dc.S3 * np.sin(3 * wt)) + dc.C3 * np.cos(3 * wt)
 
 
-def bayesian_flood_decision(dc):
+def bayesian_flood_decision(dc: xr.Dataset) -> xr.DataArray:
+    """Make a flood decision based on the prior probabilities."""
     f_prob, nf_prob = calc_prior_probability(dc)
-    decision = xr.where(
+    return xr.where(
         np.isnan(f_prob) | np.isnan(nf_prob),
         np.nan,
         np.greater(f_prob, nf_prob),
     )
-    return decision
 
 
-def bayesian_flood_probability(dc):
-    f_prob, nf_prob = calc_prior_probability(dc)
-    evidence = (nf_prob * 0.5) + (f_prob * 0.5)
-    return (f_prob * 0.5) / evidence
+def bayesian_flood_probability(dc: xr.Dataset) -> xr.DataArray:
+    """Calculate the baysian flood probability."""
+    flood_prob, nonflood_prob = calc_prior_probability(dc)
+    evidence: xr.DataArray = (nonflood_prob * 0.5) + (flood_prob * 0.5)
+    return (flood_prob * 0.5) / evidence
 
 
-def calc_prior_probability(dc):
-    nf_std = 2.754041
-    sig0 = dc.sig0
-    std = dc.STD
-    wbsc = dc.wbsc
-    hbsc = dc.hbsc
-    f_prob = (1.0 / (std * np.sqrt(2 * np.pi))) * np.exp(
-        -0.5 * (((sig0 - wbsc) / nf_std) ** 2)
+def calc_prior_probability(dc: xr.Dataset) -> tuple[xr.DataArray, xr.DataArray]:
+    """Calculate the prior probabilities for flood and non-flood."""
+    nf_std: float = 2.754041
+    sig0: xr.DataArray = dc.sig0
+    std: xr.DataArray = dc.STD
+    wbsc: xr.DataArray = dc.wbsc
+    hbsc: xr.DataArray = dc.hbsc
+    flood_prob: xr.DataArray = (1.0 / (std * np.sqrt(2 * np.pi))) * np.exp(
+        -0.5 * (((sig0 - wbsc) / nf_std) ** 2),
     )
-    nf_prob = (1.0 / (nf_std * np.sqrt(2 * np.pi))) * np.exp(
-        -0.5 * (((sig0 - hbsc) / nf_std) ** 2)
+    nonflood_prob: xr.DataArray = (1.0 / (nf_std * np.sqrt(2 * np.pi))) * np.exp(
+        -0.5 * (((sig0 - hbsc) / nf_std) ** 2),
     )
-    return f_prob, nf_prob
+    return flood_prob, nonflood_prob
--- src/dask_flood_mapper/cli.py
+++ src/dask_flood_mapper/cli.py
@@ -1,14 +1,20 @@
+"""Dask Flood Mapper CLI."""
+
+# ruff: noqa: T201
+
 import threading
 import webbrowser
 
 from dask_flood_mapper.app import app
 
 
-def open_browser():
+def open_browser() -> None:
+    """Open the web browser to the Dask Flood Mapper application."""
     webbrowser.open_new("http://127.0.0.1:5000")
 
 
-def main():
+def main() -> None:
+    """Run the Dask Flood Mapper web application."""
     threading.Timer(1.5, open_browser).start()
     app.run(debug=False)
 
